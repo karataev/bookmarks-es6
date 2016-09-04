6d22e2416bf4e93562be071edbee93c76a895d45
karataev <eugene.karataev@gmail.com>
2016-09-04 20:07:07 +0700

    Add angular and root component

---
+++ client/app/app.component.js
@@ -0,0 +1,14 @@
+import template from './app.html';
+
+class AppComponentCtrl {
+  constructor() {
+
+  }
+}
+
+const AppComponent = {
+  template,
+  controller: AppComponentCtrl
+};
+
+export default AppComponent;
--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,5 @@
-console.log('hello!');
+import angular from 'angular';
+import appComponent from './app.component';
+
+angular.module('app', [])
+  .component('app', appComponent)
